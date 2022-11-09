5d7ba1c0699188e1b85def593695d8f7be5d8a79
Kevin Sasse <sassek70@gmail.com>
2022-11-09 14:53:12 -0500

    updated give_freebie

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,8 +2,8 @@ class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
 
-    def give_freebie(dev)
-        freebie_new = Freebie.new(item_name: "Card Deck", value: "30", dev_id: dev.id, company_id: self.id)
+    def give_freebie(dev, item_name, value)
+        freebie_new = Freebie.new(item_name: item_name, value: value, dev_id: dev.id, company_id: self.id)
         freebie_new.save
     end
 
